b1ff00aefd6c7b2069591b3a9fe2b8015c4dd04e
Marc White <mwhite@overstock.com>
2016-09-12 15:15:05 -0600

    Adding f-3

---
+++ js/ClientApp.js
@@ -0,0 +1,27 @@
+/* global React ReactDOM */
+
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+  render () {
+    return (
+      div(null,
+        h1({style: {color: this.props.color}}, this.props.title)
+      )
+    )
+  }
+})
+
+var MyTitleFact = React.createFactory(MyTitle)
+var ce = React.createElement
+
+var MyFirstComponent = (
+  div(null,
+    React.createElement(MyTitle, {title: 'Use props everywhere', color: 'rebeccapurple'}),
+    MyTitleFact({title: 'Props are great!', color: 'mediumaquamarine'}),
+    ce(MyTitle, {title: 'Props are the best!', color: 'peru'})
+  )
+)
+
+ReactDOM.render(MyFirstComponent, document.getElementById('app'))
